f78eb38d7e47742375430ee9e499da8133a3acf0
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-10-02 12:15:01 +0100

    chore: initial codebase setup [-] implemented codebase structure [-] installed core packages [-] implemented linting with eslint [-] implemented formatting with prettier [-] configured typescript for project [-] added a ci check for linting and formatting [-] added pre-commit hook with husky and lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// eslint.config.js
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import pluginImport from "eslint-plugin-import";
+import pluginSecurity from "eslint-plugin-security";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: {
+        process: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+      },
+    },
+    plugins: {
+      import: pluginImport,
+      security: pluginSecurity,
+    },
+    rules: {
+      // eslint-disable-next-line no-undef
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
