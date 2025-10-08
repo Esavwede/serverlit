8274edc4f01aa9f78fd4c0862d7b5d7c98bd1cfe
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-10-08 21:24:21 +0100

    feat: implement graceful shutdown with godaddy terminus [x] tracks termination signals like sigint, sigterm [x] tracks process level errors and ends process [x] tracks server errors [x] server shuts down gracefully closing resources

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,8 @@ import app from "../app.js"
 import http from "http"
 import logger from "../common/logger/logger.js"
 import connectToDatabase from "../infrastructure/database/database.js"
+import { createTerminus } from "@godaddy/terminus"
+import mongoose from "mongoose"
 
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
@@ -15,6 +17,35 @@ server.listen(PORT, async () => {
   logger.info(`Server listening on port ${PORT}`)
 })
 
+/* Graceful Shutdown */
+
+// shutdown handler
+async function onSignal(): Promise<void> {
+  logger.info("Closing database connections")
+  await mongoose.connection.close()
+  await new Promise((resolve) => setTimeout(resolve, 200))
+  logger.info("Database connections closed")
+}
+
+// shutdown-complete handler
+async function onShutdown(): Promise<void> {
+  logger.info("Cleanup Finished, server is shutting down")
+  await new Promise((resolve) => setTimeout(resolve, 200))
+  return Promise.resolve()
+}
+
+// Grafeful Shutdown with Terminus
+createTerminus(server, {
+  signals: ["SIGINT", "SIGTERM"],
+  beforeShutdown: () => new Promise((resolve) => setTimeout(resolve, 5000)),
+  onSignal,
+  onShutdown,
+  timeout: 10000,
+})
+
+/** Error Hanlding*/
+
+// Server Error
 server.on("error", (err: NodeJS.ErrnoException) => {
   if (err.code === "EADDRINUSE") {
     logger.info(`Port ${PORT} is already in use.`)
@@ -23,4 +54,20 @@ server.on("error", (err: NodeJS.ErrnoException) => {
     logger.info(`Server error: ${err.message}`)
     process.exit(1)
   }
+})
+
+// Process-level error
+process.on("uncaughtException", (e) => {
+  logger.error(`Uncaught Exception: ${e}`)
+  process.exit(1)
+})
+
+// Process-level error
+process.on("unhandledRejection", (reason) => {
+  logger.error(`Unhandled Rejection at Promise: ${reason}`)
+
+  server.close(() => {
+    logger.info("Server closed due to unhandled promise rejection")
+    process.exit(1)
+  })
 })
